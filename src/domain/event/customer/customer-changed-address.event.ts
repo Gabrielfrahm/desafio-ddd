5c0ef4bcaf59f61aecc5689e5d28e0af90f08a6d
Gabriel Marques Frahm <gabriel_frahm@hotmail.com>
2022-11-28 12:03:45 -0300

    test: challenger

---
+++ src/domain/event/customer/customer-changed-address.event.ts
@@ -0,0 +1,13 @@
+import { EventInterface } from "../@shared/event.interface";
+
+class CustomerChangedAddressEvent implements EventInterface {
+  dataTimeOccurred: Date;
+  eventData: any;
+
+  constructor(eventData: any) {
+    this.dataTimeOccurred = this.dataTimeOccurred;
+    this.eventData = eventData;
+  }
+}
+
+export { CustomerChangedAddressEvent };
---
+++ src/domain/event/customer/customer-created.event.ts
@@ -0,0 +1,13 @@
+import { EventInterface } from "../@shared/event.interface";
+
+class CustomerCreatedEvent implements EventInterface {
+  dataTimeOccurred: Date;
+  eventData: any;
+
+  constructor(eventData: any) {
+    this.dataTimeOccurred = this.dataTimeOccurred;
+    this.eventData = eventData;
+  }
+}
+
+export { CustomerCreatedEvent };
